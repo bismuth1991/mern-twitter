ea5d279f20482b5c028180251eb65f204fce432c
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-24 17:27:03 -0800

    Add #login, #signup, #receiveSessionErrors actions and coresponding thunk actions

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -2,10 +2,48 @@ import jwtDecode from 'jwt-decode';
 import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_USER_LOGOUT = 'RECEIVE_USER_LOGOUT';
+export const RECEIVE_USER_LOGIN = 'RECEIVE_USER_LOGIN';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const loginUser = () => ({
+  type: RECEIVE_USER_LOGIN,
+});
 
 export const logoutUser = () => ({
   type: RECEIVE_USER_LOGOUT,
 });
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
+});
+
+export const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user)
+    .then(
+      () => dispatch(loginUser()),
+      err => dispatch(receiveSessionErrors(err.response.data)),
+    )
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user)
+    .then((res) => {
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      APIUtil.setAuthToken(token);
+      const decoded = jwtDecode(token);
+      dispatch(receiveCurrentUser(decoded));
+    })
+    .catch(err => dispatch(receiveSessionErrors(err.response.data)))
+);
+
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem('jwtToken');
