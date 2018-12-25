27a98d29ddfd1cfb7d5ec1788758b8bca0cc107c
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-24 16:37:51 -0800

    Make skeleton for MainPage component

---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <>
+        <h1>A Twitter Clone</h1>
+
+        <footer>
+          Copyright &copy; 2019 Chirper
+        </footer>
+      </>
+    );
+  }
+}
+
+export default MainPage;
--- frontend/src/components/root.jsx
+++ frontend/src/components/root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { instanceOf } from 'prop-types';
 
 import App from './app';
 
@@ -11,5 +12,9 @@ const Root = ({ store }) => (
     </HashRouter>
   </Provider>
 );
+
+Root.propTypes = {
+  store: instanceOf(Object).isRequired,
+};
 
 export default Root;
