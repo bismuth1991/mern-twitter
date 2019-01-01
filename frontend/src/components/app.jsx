92b0f4495a4e5432c847958524aece5fbcccc1eb
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-01 11:18:19 -0800

    Add 3 ProtectedRoute for tweets with corresponding containers

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 import MainPage from './main/main_page';
 import NavBarContainer from './nav/navbar_container';
@@ -14,6 +14,10 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+      <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
+      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
+      <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
     </Switch>
   </>
 );
